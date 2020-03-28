3d5085daa687467aaeef7f6fdc15c735cdedcf38
dev <dev@cqtechnologies.com>
2020-03-29 01:39:09 +0500

    'table_styles'

--- pricing-table/pricing-table.js
+++ pricing-table/pricing-table.js
@@ -1,12 +1,12 @@
 const getZuiTableBodyRow = (bodyItem, idx) => `
-	<tr>
+	<tr style="background-color:${idx%2==0 ? '#343a40': 'rgba(255,255,255,.05)'}">
   	<td scope="row">${bodyItem.itemName}</td>
     ${bodyItem.data.map(d => `<td>${d}</td>`).join('')}
 	</tr>
 `
 const targetPriceTable = document.getElementById('pricing-table');
 const tableHeadingHtml = `<thead><tr>${tableJsonData.headings.map((h, idx) => `
-<th scope="col"><span>${h}</span></th>`).join('')}</tr></thead>`;
+<th style="background-color:rgba(255,255,255,.05)" scope="col"><span>${h}</span></th>`).join('')}</tr></thead>`;
 const tableBodyHtml = `<tbody>${tableJsonData.body.map(getZuiTableBodyRow).join('')}</tbody>`
 
 const colWidth = `<colgroup>${tableJsonData.colWidth.map((width) => `<col width=${width}/>`).join('')}</colgroup>`;
