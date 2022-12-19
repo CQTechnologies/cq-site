e2e020897c19ff8755e069bbadc95c06da41b819
wjd <wjd@wjd.wjd>
2022-12-19 12:56:52 +0500

    fixed all in slider

--- js/image-sliderjs/image-slider.js
+++ js/image-sliderjs/image-slider.js
@@ -1,52 +1,30 @@
 
-let slideIndex = 0;
+let CURRENT_SLIDE_INDEX = 0;
+let addedSlides = []
 
 // show|hide slider onClick event
-function toggleSlider(n) {
-  let slider = document.getElementById("slider-images-container");
-  if(slider !== null) {
-    slider.style = "display:" + n + ";";
+function toggleSlider(displayChange, slides) {
+  let imageSlider = document.getElementById("slider-images-container");
+  if(imageSlider !== undefined && imageSlider !== null) {
+    imageSlider.style = "display:" + displayChange + ";";
+    if(displayChange === "block") {
+      addedSlides = slides;
+      if(slides && slides.length > 0) document.getElementById("current-slide").src = slides[0];
+    }
   }
 }
 
-const dt = [
-  `https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg`,
-  `https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg`,
-  `https://cdn.pixabay.com/photo/2012/03/01/00/55/flowers-19830__340.jpg`,
-  `./../img/pricing-table.png`,
-]
-function showSlides(n) {
-  console.log(dt.length, n)
-  const index = slideIndex + n;
-  if (index > dt.length -1) {slideIndex = 0}
-  else if (index < 0) {slideIndex = dt.length -1}
-  else slideIndex = index;
+function showSlides(slideNo) {
+  const copiedSlideIndex = CURRENT_SLIDE_INDEX + slideNo;
+  
+  if (copiedSlideIndex > addedSlides.length -1) {CURRENT_SLIDE_INDEX = 0}
+  else if (indcopiedSlideIndexex < 0) {CURRENT_SLIDE_INDEX = addedSlides.length -1}
+  else CURRENT_SLIDE_INDEX = copiedSlideIndex;
   
   let currentSlide = document.getElementById("current-slide");
   let slideNumber = document.getElementById("slide-number");
   let slideText = document.getElementById("slide-text");
-  currentSlide.src = dt[slideIndex];
-  slideNumber.innerHTML = (slideIndex + 1) + " / " + dt.length;
-  slideText.innerHTML = (slideIndex + 1) + " / " + dt.length;
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides2(n) {
-  let i;
-  let slides = document.getElementsByClassName("slider-mySlides");
-  let slider_dots = document.getElementsByClassName("slider-dots");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < slider_dots.length; i++) {
-    slider_dots[i].className = slider_dots[i].className.replace(" slider-active-slide", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  slider_dots[slideIndex-1].className += " slider-active-slide";
+  currentSlide.src = addedSlides[CURRENT_SLIDE_INDEX];
+  slideNumber.innerHTML = (CURRENT_SLIDE_INDEX + 1) + " / " + addedSlides.length;
+  slideText.innerHTML = ""; // "Images descriptions can be added here.";
 }
